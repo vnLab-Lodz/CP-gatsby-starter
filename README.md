098d0d627cc1e74382e2cf9605646e26ce0f2b1f
yurii-dev-shcheholiev <yurii.shcheholiev@gmail.com>
2020-11-23 14:49:21 +0200

    fixed the plugins

---
+++ README.md
@@ -0,0 +1,7 @@
+First install all the gatsby plugins with npm with the following command: 
+
+npm install gatsby-plugin-react-helmet gatsby-source-filesystem gatsby-transformer-remark gatsby-plugin-typescript gatsby-plugin-sass gatsby-plugin-manifest gatsby-plugin-offline
+
+Secondly install all the dependent modules:
+
+npm install gatsby react react-dom react-helmet
--- gatsby-config.js
+++ gatsby-config.js
@@ -7,57 +7,58 @@
 module.exports = {
   /* Your site config here */
   plugins: [
-    // `gatsby-plugin-react-helmet`,
-    // {
-    //   resolve: `gatsby-source-filesystem`,
-    //   options: {
-    //     name: `pages`,
-    //     path: `${__dirname}/src/pages/`,
-    //   },
-    // },
-    // {
-    //   resolve: `gatsby-source-filesystem`,
-    //   options: {
-    //     name: `data`,
-    //     path: `${__dirname}/src/data/`,
-    //     ignore: [`**/\.*`], // ignore files starting with a dot
-    //   },
-    // },
-    // {
-    //   resolve: `gatsby-transformer-remark`,
-    //   options: {
-    //     // CommonMark mode (default: true)
-    //     commonmark: true,
-    //     // Footnotes mode (default: true)
-    //     footnotes: true,
-    //     // Pedantic mode (default: true)
-    //     pedantic: true,
-    //     // GitHub Flavored Markdown mode (default: true)
-    //     gfm: true,
-    //     // Plugins configs
-    //     plugins: [],
-    //   },
-    // },
-    // {
-    //   resolve: `gatsby-plugin-typescript`,
-    //   options: {
-    //     isTSX: true, // defaults to false
-    //     jsxPragma: `jsx`, // defaults to "React"
-    //     allExtensions: true, // defaults to false
-    //   },
-    // },
-    // `gatsby-plugin-sass`,
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: `GatsbyJS`,
-    //     short_name: `GatsbyJS`,
-    //     start_url: `/`,
-    //     background_color: `#555`,
-    //     theme_color: `#096`,
-    //     display: `standalone`,
-    //   },
-    // },
-    // `gatsby-plugin-offline`
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data/`,
+        ignore: [`**/\.*`], // ignore files starting with a dot
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        // CommonMark mode (default: true)
+        commonmark: true,
+        // Footnotes mode (default: true)
+        footnotes: true,
+        // Pedantic mode (default: true)
+        pedantic: true,
+        // GitHub Flavored Markdown mode (default: true)
+        gfm: true,
+        // Plugins configs
+        plugins: [],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-typescript`,
+      options: {
+        isTSX: true, // defaults to false
+        jsxPragma: `jsx`, // defaults to "React"
+        allExtensions: true, // defaults to false
+      },
+    },
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#555`,
+        theme_color: `#096`,
+        display: `standalone`,
+        icon: `src/images/icon.png`
+      },
+    },
+    `gatsby-plugin-offline`
 ],
 }
