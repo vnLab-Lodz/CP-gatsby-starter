cad005f31a023caed94b95443a2d446da932b3e2
Mariusz <mariuszpisarski0@gmail.com>
2020-11-29 15:56:38 +0100

    index and anotherPage

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,4 @@
 import React from "react"
 export default function Header(props) {
-  return <h1>This is a header.</h1>
   return <h1>{props.headerText}</h1>
-
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import React from "react"
-
-export default () =>  {
+import { Link } from "gatsby"
+import Header from "../components/header"
+export default function Home() {
   return (
-  <div style={{ color: `purple` }}>
-    <script src="https://hypothes.is/embed.js" async/>
-    <p>Hello world!</p>
-  </div>
+    <div style={{ color: `purple` }}>
+      <script src="https://hypothes.is/embed.js" async/>
+      <Header headerText="Hello World!" />
+      <p>What a nice image!!!</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+      <br/>
+      <Link to="/anotherPage/">AnotherPage</Link>
+    </div>
   )
 }
