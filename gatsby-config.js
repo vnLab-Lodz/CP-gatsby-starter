f972ba6ad1417df9554093c734ac1db4b4fb44e1
Mariusz <mariuszpisarski0@gmail.com>
2020-12-02 23:29:26 +0100

    gatsby-plugin-sharp and gatsby-transformer-sharp

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,8 @@
 module.exports = {
   /* Your site config here */
   plugins: [
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
