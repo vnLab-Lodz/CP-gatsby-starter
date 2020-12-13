3726f904d55fe0a26d3dd3117cd11de439cd7a42
yurii-dev-shcheholiev <yurii.shcheholiev@gmail.com>
2020-12-13 14:48:56 +0200

    md1

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
 import React from "react"
-import { Link } from "gatsby"
+import { css } from "@emotion/react"
+import { rhythm } from "../utils/typography"
+import { Link, graphql } from "gatsby"
 import { Helmet } from 'react-helmet'
 import Header from "../components/header"
 import "../stylesheets/scssExample.scss"
-export default function Home() {
+
+export default function Home({data}) {
   return (
 
     <div style={{ color: `purple` }}>
@@ -11,9 +14,48 @@ export default function Home() {
       <script src="https://hypothes.is/embed.js" async/>
       <Header headerText="Hello World!" />
       <p>What a nice image!!!</p>
-      <img src="https://source.unsplash.com/random/400x200" alt="" />
+
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            <h3
+              css={css`
+                margin-bottom: ${rhythm(1 / 4)};
+              `}
+            >
+              {node.frontmatter.title}{" "}
+              <span
+                css={css`
+                  color: #bbb;
+                `}
+              >
+                — {node.frontmatter.date}
+              </span>
+            </h3>
+            <p>{node.excerpt}</p>
+          </div>
+        ))}
       <br/>
       <Link to="/anotherPage/">AnotherPage</Link>
     </div>
   )
 }
+
+//https://source.unsplash.com/random/400x200"
+// fluid={data.imageFile.childImageSharp.fluid}
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
---
+++ src/pages/markdownPage.md
@@ -0,0 +1,9 @@
+---
+title: "MarkDown to HTML generated page"
+date: "2020-12-13"
+---
+Here is an example of a simple html page generated from a MarkDown file.
+Don't hesitate to have a look in a code.
+
+And a random panda video, so you see markdown works with media
+<iframe width="560" height="315" src="https://www.youtube.com/embed/4n0xNbfJLR8" frameborder="0" allowfullscreen></iframe>
