04ec0a56fcda3ca2f39bdbd5658b874e696456a6
Mariusz <mariuszpisarski0@gmail.com>
2020-12-13 14:55:07 +0100

    update: simple page design created

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,17 +3,38 @@ import { Link } from "gatsby"
 import { Helmet } from 'react-helmet'
 import Header from "../components/header"
 import "../stylesheets/scssExample.scss"
+import { graphql } from "../../.cache/gatsby-browser-entry"
 export default function Home() {
   return (
-
-    <div style={{ color: `purple` }}>
-      <Helmet title="foo bar" defer={false} />  {/*defer={false} is to overcome an upstream issue with React Helmet*/}
-      <script src="https://hypothes.is/embed.js" async/>
-      <Header headerText="Hello World!" />
-      <p>What a nice image!!!</p>
-      <img src="https://source.unsplash.com/random/400x200" alt="" />
-      <br/>
-      <Link to="/anotherPage/">AnotherPage</Link>
+    <div>
+      <div id="content">
+        <Helmet title="Very " defer={false} />  {/*defer={false} is to overcome an upstream issue with React Helmet*/}
+        <script src="https://hypothes.is/embed.js" async/>
+        <Header headerText="Hello World!" />
+        <p>What a nice image!!!</p>
+        <div>
+        <h2>Example header!!!</h2>
+        </div>
+        <img src="https://source.unsplash.com/random/400x200" alt="" />
+        <br/>
+        <Link to="/anotherPage/">AnotherPage</Link>
+      </div>
+      <div className="credentials">
+        <div>
+          <div className="author-header">
+            Authors:
+          </div>
+          <div className="authors">
+            <div className="author">
+              Mariusz Pisarski
+            </div>
+            <div className="author">
+              Yura Shcheholiev
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
+
