bdc7502581ed69bdf1b7e93dddb6b4f0f978be4a
yurii-dev-shcheholiev <yurii.shcheholiev@gmail.com>
2020-12-15 22:16:13 +0200

    merged index + mrd to html pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 import { Helmet } from 'react-helmet'
 import Header from "../components/header"
 import "../stylesheets/scssExample.scss"
-import { graphql } from "../../.cache/gatsby-browser-entry"
-export default function Home() {
+
+export default function Home({data}) {
   return (
     <div>
       <div id="content">
@@ -13,12 +13,30 @@ export default function Home() {
         <Header headerText="Hello World!" />
         <p>What a nice image!!!</p>
         <div>
-        <h2>Example header!!!</h2>
+          <h2>Example header!!!</h2>
         </div>
         <img src="https://source.unsplash.com/random/400x200" alt="" />
         <br/>
         <Link to="/anotherPage/">AnotherPage</Link>
+
+        <div id="content">
+          
+          {data.allMarkdownRemark.edges.map(({ node }) => (
+              <div key={node.id}>
+                <Link to={node.fields.slug}>
+                  <h3>
+                    {node.frontmatter.title}{" "}
+                    <span>
+                      — {node.frontmatter.date}
+                    </span>
+                  </h3>
+                  <p>{node.excerpt}</p>
+                </Link>
+              </div>
+            ))}
+        </div>
       </div>
+
       <div className="credentials">
         <div>
           <div className="author-header">
@@ -37,3 +55,26 @@ export default function Home() {
     </div>
   )
 }
+
+//https://source.unsplash.com/random/400x200"
+// fluid={data.imageFile.childImageSharp.fluid}
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
